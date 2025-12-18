bb5f34629b1dad3c73d623080c089660089606d8
Trill <trillsxpp@gmail.com>
2025-12-18 16:42:04 -0500

    Header update

--- app/header.tsx
+++ app/header.tsx
@@ -30,7 +30,7 @@ export function Header() {
           className="text-zinc-600 dark:text-zinc-500"
           delay={0.5}
         >
-          MSIS Candidate | AI For Business Innovation
+          MS Information Systems Candidate | AI For Business Innovation
         </TextEffect>
       </div>
       {mounted && (
